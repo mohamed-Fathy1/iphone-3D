e522ab23d9c168e814d6b2179b4f1d0ab757c770
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-06-22 19:51:57 +0300

    Add video carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="w-full nav-height bg-black relative">
+    <section className="w-full nav-height overflow-hidden bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
           iphone 15 Pro
