7358c692ccf50d314cfc2911b32ebea46ad684be
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-06-22 04:56:16 +0300

    create hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,64 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+    gsap.to("#hero-video", {
+      clipPath: "circle(100% at 50% 50%)",
+      duration: 1.5,
+      ease: "power4.in",
+    });
+    gsap.to("#cta", { y: -50, opacity: 1, duration: 1, delay: 2 });
+  }, []);
+
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth > 760 ? heroVideo : smallHeroVideo
+  );
+
+  const handelVideoSize = () =>
+    setVideoSrc(window.innerWidth > 760 ? heroVideo : smallHeroVideo);
+
+  useEffect(() => {
+    window.addEventListener("resize", handelVideoSize);
+
+    return () => window.removeEventListener("resize", handelVideoSize);
+  }, []);
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iphone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iphone 15 Pro
+        </p>
+        <div
+          id="hero-video"
+          className="md:w-10/12 w-9/12"
+          style={{ clipPath: "circle(17% at 50% 50%)" }}
+        >
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center translate-y-20 opacity-0"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const Highlights = () => {
-  return <div>Highlights</div>;
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0, duration: 1 });
+  }, []);
+
+  return (
+    <div
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the highlights
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Highlights;
